fede9c77c5c3516343f043ee57ca4680a8b1ecdb
shashikant kumar <kumarshashikant05@gmail.com>
2023-05-01 17:17:46 +0530

    small changes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRouter = require("./Routes/Auth");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use((req, res, next) => {
   res.set({
     "Access-Control-Allow-Origin": "*",
